1f9e63ce923696ffcdbc89460a0c778b6d3a6f87
Arnaud293 <arnaudlt293@gmail.com>
2022-09-29 11:31:27 +0200

    add localstorage for to do

--- index.js
+++ index.js
@@ -4,25 +4,41 @@ const container = document.querySelector('.container');
 
 const dataStorage = localStorage;
 
+const listStorage = () => {
+    window.localStorage.toDoList = list.innerHTML;
+}
 
+const getList = () => {
+
+    if(window.localStorage.toDoList){
+        list.innerHTML = window.localStorage.toDoList;
+    }
+    else {
+        list.innerHTML = `<li>Cliquez sur un to-do pour le valider/supprimer</li>`
+    }
+    
+}
+
+getList();
 
 form.addEventListener('submit', (e) => {
 
     e.preventDefault();
 
-    console.log(input.value);
     list.innerHTML += `<li>${input.value}</li>`;
-    window.localStorage.setItem('dataStorage', list.innerHTML)
     item.value = null;
+    listStorage();
 })
 
 list.addEventListener('click', (e) => {
 
     if(!e.target.classList.contains('checked')){
         e.target.classList.add('checked');
+        listStorage();
 
     } else {
         e.target.remove();
+        listStorage();
     }
 
     
